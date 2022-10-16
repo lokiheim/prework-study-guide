03631f7e16bd1bc0407da8a989d399703d1d6b34
iain macdonald <loki@mail.com>
2022-10-16 23:28:26 +0100

    added some javaScript

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var topics = ["HTML", "CSS", "Git","JavaScript"];
+/*
+if (topic === "HTML") {
+    console.log("Lets study HTML!");
+} else if (topic === "CSS") {
+    console.log("Lets study CSS!");
+} else if (topic === "Git") {
+    console.log("Lets study Git!");
+} else if (topic === "JavaScript") {
+    console.log("Lets study JavaScript!");
+} else {
+    console.log("Please try again");
+}
+*/
+for(var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+    
+}
